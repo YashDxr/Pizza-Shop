ed6a5e7d2f9445a1f99ccdc047a963ae50727843
Yash Singh <108719370+YashDxr@users.noreply.github.com>
2023-09-12 18:59:23 +0530

    Update config.js

--- scripts/utils/config.js
+++ scripts/utils/config.js
@@ -1,4 +1,4 @@
 // Fake Data for prior development till backend is not ready . We use the below URL from git as fake data.
 // Never use URL in code 
 // Configurations are never part of code
-export const URL = 'http://127.0.0.1:5500/server.json' ;
+export const URL = 'https://yashdxr.github.io/Pizza-Shop/server.json' ;
